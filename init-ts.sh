c0586ac9960720416a55d7ab64a9ed0f5fdd1d89
Dan Cohen <dan.cohen@cazoo.co.uk>
2019-10-30 10:11:17 +0000

    add the typescript project initialation script

---
+++ init-ts.sh
@@ -0,0 +1,27 @@
+!# /bin/bash
+
+npm init -y
+
+npm install -D typescript
+npm install -D tslint
+
+npm install express -S
+npm install @types/express -D
+
+echo '{
+  "compilerOptions": {
+    "module": "commonjs",
+    "esModuleInterop": true,
+    "target": "es6",
+    "moduleResolution": "node",
+    "sourceMap": true,
+    "outDir": "dist"
+  },
+  "lib": ["es2015"]
+}' > tsconfig.json
+
+./node_modules/.bin/tslint --init
+
+mkdir src
+touch src/app.ts
+echo "expect(true).toBe(false);" > src/app.test.ts
