bd46c4bbc0340cc05037711bfad66a1cbbd14e27
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-06 00:19:13 +0100

    maybe now?

--- rename-commit-history.sh
+++ rename-commit-history.sh
@@ -1,8 +1,8 @@
 export FILTER_BRANCH_SQUELCH_WARNING=1
 git filter-branch --env-filter '
-OLD_EMAIL=${1}
-NEW_NAME=${2}
-NEW_EMAIL=${3}
+OLD_EMAIL=$1
+NEW_NAME=$2
+NEW_EMAIL=$3
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
   export GIT_COMMITTER_NAME="$NEW_NAME"
