51dc06e40fddadcc843c5f6ee6e996821ae725e8
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-06 00:31:30 +0100

    force - no need for backups lesss goooo

--- rename-commit-history.sh
+++ rename-commit-history.sh
@@ -1,5 +1,5 @@
 export FILTER_BRANCH_SQUELCH_WARNING=1
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 OLD_EMAIL='"$1"'
 NEW_NAME="'"$2"'"
 NEW_EMAIL='"$3"'
@@ -13,4 +13,4 @@ then
   export GIT_AUTHOR_NAME="$NEW_NAME"
   export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
 fi
-' --tag-name-filter cat -- --branches --tags
+' --tag-name-filter cat -- --branches --tags 
